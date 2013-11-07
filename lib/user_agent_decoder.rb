2c41853f0df3c871501ba60cf39d4eb772079b00
Matt Bearman <matt@mattbearman.co.uk>
2013-11-07 18:29:45 +0000

    First

---
+++ lib/user_agent_decoder.rb
@@ -0,0 +1,99 @@
+class UserAgentDecoder
+	require 'yaml'
+
+	attr_reader :result
+	attr_writer :user_agent_string
+
+	def initialize user_agent_string = nil
+		load_rules
+
+		@result = {
+			os: {
+				token: '',
+				name: '',
+				version: '',
+				version_name: ''
+			},
+			browser: {
+				name: '',
+				version: ''
+			}
+		}
+
+		if user_agent_string && user_agent_string.strip != ''
+			@user_agent_string = user_agent_string
+		end
+	end
+
+	def parse
+		find_os
+		find_os_version_name
+		find_browser
+
+		@result
+	end
+
+	def find_os
+		catch :found_os do
+			@rules['os'].each do | token, os |
+				os['regex'].each do | regex |
+
+					matches = Regexp.new(regex, 'i').match @user_agent_string
+
+					if matches
+						@result[:os][:token] = token
+						@result[:os][:name] = os['name']
+						
+						if matches[1]
+							@result[:os][:version] = matches[1].gsub '_', '.'
+						end
+
+						throw :found_os
+					end
+				end
+			end
+		end
+	end
+
+	def find_os_version_name
+		catch :found_os_version_name do
+			version_names = @rules['os_version_names'][@result[:os][:token]]
+			
+			if version_names 
+				version_names.each do | version_name |
+					if Regexp.new("^#{version_name['version']}").match @result[:os][:version]
+						@result[:os][:version_name] = version_name['name']
+
+						throw :found_os_version_name
+					end
+				end
+			end
+		end
+	end
+
+	def find_browser
+		catch :found_browser do
+			if @result[:os][:token] && @result[:os][:token].strip != ''
+				browsers = @rules['os_browsers'][@result[:os][:token]].each do | browser |
+					@rules['browsers'][browser]['regex'].each do | regex |
+						matches = Regexp.new(regex, 'i').match @user_agent_string
+
+						if matches
+							@result[:browser][:name] = @rules['browsers'][browser]['name']
+							
+							if matches[1]
+								@result[:browser][:version] = matches[1].gsub '_', '.'
+							end
+
+							throw :found_browser
+						end
+					end
+				end
+			end
+		end
+	end
+
+	def load_rules
+		@rules = YAML.load_file "lib/user_agent_rules.yml"
+	end
+end
---
+++ user_agent_decoder.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'user_agent_decoder'
+  s.version     = '0.0.1'
+  s.date        = '2013-11-06'
+  s.summary     = "User Agent Decoder"
+  s.description = "Accurately parse User Agent strings using simple, cascading rules - devloped for and by BugMuncher"
+  s.authors     = ["Matt Bearman"]
+  s.email       = 'info@bugmuncher.com'
+  s.files       = ["lib/user_agent_decoder.rb"]
+  s.homepage    = 'http://rubygems.org/gems/user_agent_decoder'
+  s.license     = 'MIT'
+end
